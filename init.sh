747e2d1221e9e42b31664ca4dff2480dc336a68d
Yoriyasu Yano <yorinasub17@gmail.com>
2013-08-22 04:33:44 -0700

    Add routine to make command t to init script

--- init.sh
+++ init.sh
@@ -4,3 +4,8 @@ git submodule init
 git submodule update
 git submodule foreach git submodule init
 git submodule foreach git submodule update
+
+# Build command t
+cd bundle/command-t/ruby/command-t
+ruby extconf.rb
+make
